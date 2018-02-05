4a7ad1d78e111d2c59d75817d917d7c32ff88637
Taqi Jaffri <tjaffri@gmail.com>
2018-02-05 15:03:22 -0800

    Update setup.ps1

--- setup.ps1
+++ setup.ps1
@@ -1,4 +1,4 @@
-# Install Chocolatey (snippet from https://chocolatey.org/install#install-with-powershellexe)
+[Environment]::SetEnvironmentVariable("Path", "C:\ProgramData\Miniconda3\Scripts;" + $env:Path, [EnvironmentVariableTarget]::Machine)# Install Chocolatey (snippet from https://chocolatey.org/install#install-with-powershellexe)
 Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 
 # Install git
@@ -6,6 +6,7 @@ choco install git -y
 
 # Install miniconda3
 choco install miniconda3 -y
+[Environment]::SetEnvironmentVariable("Path", "C:\ProgramData\Miniconda3\Scripts;" + $env:Path, [EnvironmentVariableTarget]::Machine)
 
 # Install vscode, and related extensions/plugins
 choco install visualstudiocode --params '/NoDesktopIcon' -y
