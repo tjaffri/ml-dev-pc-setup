1b0efaa006228aa580b22e199cbf1eaff7753ba4
Taqi Jaffri <tjaffri@gmail.com>
2018-02-05 15:58:37 -0800

    Update setup.ps1

--- setup.ps1
+++ setup.ps1
@@ -6,7 +6,16 @@ choco install git -y
 
 # Install miniconda3
 choco install miniconda3 -y
+
+# Set the path for miniconda3 scripts
 [Environment]::SetEnvironmentVariable("Path", "C:\ProgramData\Miniconda3\Scripts;" + $env:Path, [EnvironmentVariableTarget]::Machine)
+
+# We are installing in an elevated prompt (chocolatey likes that) however we don't want the miniconda folder to be admin only
+# since then we will subsequently always need to work with conda in elevated prompts. As a hack, fix up the permissions for that folder
+$Acl = Get-ACL "C:\ProgramData\Miniconda3"
+$AccessRule= New-Object System.Security.AccessControl.FileSystemAccessRule("everyone","full","ContainerInherit,Objectinherit","none","Allow")
+$Acl.AddAccessRule($AccessRule)
+Set-Acl $SharePath $Acl
 
 # Install vscode, and related extensions/plugins
 choco install visualstudiocode --params '/NoDesktopIcon' -y
