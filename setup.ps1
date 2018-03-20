daf725c8448dd9a22783ffed88a283337ea34c72
Taqi Jaffri <tjaffri@gmail.com>
2018-03-20 16:43:03 -0700

    Update setup.ps1

--- setup.ps1
+++ setup.ps1
@@ -14,11 +14,11 @@ choco install nodejs -y
 choco install miniconda3 -y
 
 # Set the path for miniconda3 scripts
-[Environment]::SetEnvironmentVariable("Path", "C:\ProgramData\Miniconda3\Scripts;" + $env:Path, [EnvironmentVariableTarget]::Machine)
+[Environment]::SetEnvironmentVariable("Path", "C:\tools\miniconda3\Scripts;" + $env:Path, [EnvironmentVariableTarget]::Machine)
 
 # We are installing in an elevated prompt (chocolatey likes that) however we don't want the miniconda folder to be admin only
 # since then we will subsequently always need to work with conda in elevated prompts. As a hack, fix up the permissions for that folder
-$MinicondaPath = "C:\ProgramData\Miniconda3"
+$MinicondaPath = "C:\tools\miniconda3"
 $Acl = Get-ACL $MinicondaPath
 $AccessRule= New-Object System.Security.AccessControl.FileSystemAccessRule("Everyone", "Full", "ContainerInherit, ObjectInherit" , "None" , "Allow")
 $Acl.AddAccessRule($AccessRule)
